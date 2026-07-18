758c2a3c40c942409f7d517c7692f8bf0c92ceef
Igor Novokshonov <gogainda@yandex.ru>
2026-07-18 19:28:48 +0200

    Add mdview-cli formula

---
+++ Formula/mdview-cli.rb
@@ -0,0 +1,65 @@
+class MdviewCli < Formula
+  include Language::Python::Virtualenv
+
+  desc "Preview Markdown files with mdview.io from the terminal"
+  homepage "https://mdview.io"
+  url "https://files.pythonhosted.org/packages/47/12/20f507ca34e77620896acd275e012f1d9e63c133847e9cd65633dc7f1bb9/mdview_cli-0.1.0.tar.gz"
+  sha256 "b2112330fb3dbc148053862a6eddf179fa3c7f2568c38c4867a852bf708e4733"
+  license "MIT"
+
+  depends_on "python@3.13"
+
+  resource "anyio" do
+    url "https://files.pythonhosted.org/packages/61/cc/a381afa6efea9f496eff839d4a6a1aed3bfafc7b3ab4b0d1b243a12573dd/anyio-4.14.2.tar.gz"
+    sha256 "cfa139f3ed1a23ee8f88a145ddb5ac7605b8bbfd8592baacd7ce3d8bb4313c7f"
+  end
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/c9/c7/424b75da314c1045981bd9777432fad05a9e0c69daa4ed7e308bbaffe405/certifi-2026.6.17.tar.gz"
+    sha256 "024c88eeec92ca068db80f02b8b07c9cef7b9fe261d1d535abfd5abd6f6af432"
+  end
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/76/d4/81420972a676e8ffea40450d8c8c92943e7218a78fe9b64359836cc9876b/click-8.4.2.tar.gz"
+    sha256 "9a6cea6e60b17ebe0a44c5cc636d94f09bd66142c1cd7d8b4cd731c4917a15f6"
+  end
+
+  resource "h11" do
+    url "https://files.pythonhosted.org/packages/01/ee/02a2c011bdab74c6fb3c75474d40b3052059d95df7e73351460c8588d963/h11-0.16.0.tar.gz"
+    sha256 "4e35b956cf45792e4caa5885e69fba00bdbc6ffafbfa020300e549b208ee5ff1"
+  end
+
+  resource "httpcore" do
+    url "https://files.pythonhosted.org/packages/06/94/82699a10bca87a5556c9c59b5963f2d039dbd239f25bc2a63907a05a14cb/httpcore-1.0.9.tar.gz"
+    sha256 "6e34463af53fd2ab5d807f399a9b45ea31c3dfa2276f15a2c3f00afff6e176e8"
+  end
+
+  resource "httpx" do
+    url "https://files.pythonhosted.org/packages/b1/df/48c586a5fe32a0f01324ee087459e112ebb7224f646c0b5023f5e79e9956/httpx-0.28.1.tar.gz"
+    sha256 "75e98c5f16b0f35b567856f597f06ff2270a374470a5c2392242528e3e3e42fc"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/cd/63/9496c57188a2ee585e0f1db071d75089a11e98aa86eb99d9d7618fc1edce/idna-3.18.tar.gz"
+    sha256 "ffb385a7e039654cef1ab9ef32c6fafe283c0c0467bba1d9029738ce4a14a848"
+  end
+
+  resource "platformdirs" do
+    url "https://files.pythonhosted.org/packages/52/cd/4f25b2f95b23f5d2c9c1fe43e49841bff5800562149b2666afc09309aa8f/platformdirs-4.10.1.tar.gz"
+    sha256 "ceab4084426fe6319ce18e86deada8ab1b7487c7aee7040c55e277c9ae793695"
+  end
+
+  resource "sniffio" do
+    url "https://files.pythonhosted.org/packages/a2/87/a6771e1546d97e7e041b6ae58d80074f81b7d5121207425c964ddf5cfdbd/sniffio-1.3.1.tar.gz"
+    sha256 "f4324edc670a0f49750a81b895f35c3adb843cca46f0530f79fc1babb23789dc"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_match "mdv, version #{version}", shell_output("#{bin}/mdv --version")
+    assert_match "No token configured", shell_output("#{bin}/mdv keys list")
+  end
+end
---
+++ README.md
@@ -0,0 +1,22 @@
+# gogainda/homebrew-tap
+
+Homebrew formulae for [gogainda](https://github.com/gogainda)'s tools.
+
+## Install
+
+```bash
+brew install gogainda/tap/mdview-cli
+```
+
+or
+
+```bash
+brew tap gogainda/tap
+brew install mdview-cli
+```
+
+## Formulae
+
+| Formula | Description |
+|---|---|
+| `mdview-cli` | Preview Markdown files with [mdview.io](https://mdview.io) from the terminal (`mdv`) |
